d0a89bf39836a002eab1048e240ebcf45d3d5a37
gomezluismiguel <gomezpaezluismiguel@gmail.com>
2020-05-04 19:58:12 -0500

    modelo de Projecto

---
+++ src/controlador/Principal.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package controlador;
+
+/**
+ * @author Admin
+ *
+ */
+public class Principal {
+
+	/**
+	 * En esta pagina tendremos el metodo principal de java (main)
+	 */
+	public Principal() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
